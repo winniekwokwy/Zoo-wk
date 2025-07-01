049ff9bcfa078d743e6ee702eb73fd307ac4a1de
winniekwokwy <206894518+winniekwokwy@users.noreply.github.com>
2025-07-01 08:40:19 +0100

    move all request models under request folder

--- obj/Debug/net8.0/Zoo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Zoo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08ecbbf85a1bd7a9800997b9767eeb0edd621905")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+241ae807c0ac773bbe8744772eeb8aed33797203")]
 [assembly: System.Reflection.AssemblyProductAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Zoo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
